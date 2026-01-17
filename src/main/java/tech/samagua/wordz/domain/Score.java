14874aa6a648399551a6456f1a02e58737ea0771
Stalin Amagua <12144966+jueltter@users.noreply.github.com>
2026-01-17 00:39:50 -0500

    refactor 3

--- src/main/java/tech/samagua/wordz/domain/Score.java
+++ src/main/java/tech/samagua/wordz/domain/Score.java
@@ -3,13 +3,14 @@ package tech.samagua.wordz.domain;
 public class Score {
     private final String correct;
     private Letter result = Letter.INCORRECT;
+    private int position;
 
     public Score(String correct) {
         this.correct = correct;
     }
 
     public void assess(int position, String attempt) {;
-        if (isCorrectLetter(position, attempt)) {
+        if (isCorrectLetter(attempt)) {
             result = Letter.CORRECT;
 
         }
@@ -19,7 +20,7 @@ public class Score {
         return result;
     }
 
-    private boolean isCorrectLetter(int position, String attempt) {
+    private boolean isCorrectLetter(String attempt) {
         return correct.charAt(position) == attempt.charAt(position);
     }
 }
